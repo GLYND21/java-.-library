77f57f77f4c805e4965a5da9b85e8661ac9b759c
not3amer <103201013+not3amer@users.noreply.github.com>
2022-12-24 20:59:42 +0200

    Update addNewBook.java

--- src/addNewBook.java
+++ src/addNewBook.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,7 +8,7 @@ import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
 /**
  *
- * @author DELL
+ * @author ali
  */
 public class addNewBook extends javax.swing.JFrame {
 
